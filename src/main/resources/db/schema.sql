d0df4886220419ec395d340f819ced3f3e42fbba
suocaiyuan <suocaiyuan@163.com>
2023-04-01 12:14:17 +0800

    增加配置文件

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,3 @@
+drop table student if exists;
+
+create table student (USER_ID int not null primary key auto_increment, USER_NAME varchar(50));
--- src/main/resources/db/table.sql
+++
@@ -1,4 +0,0 @@
-create table if not exists 'student'(
-                                   USER_ID int not null primary key auto_increment,
-                                   USER_NAME varchar(50),
-);
